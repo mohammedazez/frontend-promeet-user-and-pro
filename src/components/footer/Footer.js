57a9de59109b4db01590701352749725b932808f
azezmohammed <muhamadaziz047@gmail.com>
2020-11-05 10:11:56 +0700

    done for home

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,33 +1,53 @@
 import React from "react";
 import "./Footer.css";
 import { Col, Row } from "react-bootstrap";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 function footer() {
   return (
     <div className="container-footer">
-      <Row>
+      <Row className="column-footer">
         <Col>
-          <h4>Promeet</h4>
-          <p>Berita & Promo</p>
-          <p>Blog</p>
+          <h4 className="judul-footer-kiri">Promeet</h4>
+          <p className="kalimat-footer-kiri">Berita & Promo</p>
+          <p className="kalimat-footer-kiri">Blog</p>
         </Col>
         <Col>
-          <h4>Bantuan & Panduan</h4>
-          <p>Bantuan</p>
-          <p>Kebijakan & Privasi</p>
+          <h4 className="judul-footer-kiri">Bantuan & Panduan</h4>
+          <p className="kalimat-footer-kiri">Bantuan</p>
+          <p className="kalimat-footer-kiri">Kebijakan & Privasi</p>
         </Col>
         <Col>
-          <h4>Ikuti Kami</h4>
+          <h4 className="judul-footer-kiri">Ikuti Kami</h4>
+          <Row>
+            <AiFillFacebook className="social-footer" />
+            <AiFillInstagram className="social-footer" />
+            <AiFillTwitterCircle className="social-footer" />
+            <AiFillYoutube className="social-footer" />
+          </Row>
         </Col>
-        <Col>
-          <h4>Download Promeet</h4>
-          <img
-            src="https://res.cloudinary.com/def4tydoe/image/upload/v1604510708/download_in_google_play_and_app_store_rgbxgf.png"
-            alt="download"
-            className="img-footer"
-          />
+        <Col className="test">
+          <h4 className="judul-footer-kanan">Download Promeet</h4>
+          <div className="img-footer">
+            <img
+              src="https://res.cloudinary.com/def4tydoe/image/upload/v1604542117/download_hrtfsh.svg"
+              alt="googlestore"
+            />
+            <img
+              src="https://res.cloudinary.com/def4tydoe/image/upload/v1604542118/download_1_ovbr82.svg"
+              alt="appstore"
+            />
+          </div>
         </Col>
       </Row>
+      <p className="copyright">
+        Copyright © 2020 Promeet. All rights reserved.
+      </p>
     </div>
   );
 }
