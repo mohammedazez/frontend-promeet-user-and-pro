bef2a08d65e1f6338f756ec2f79808fabe1815be
azezmohammed <muhamadaziz047@gmail.com>
2020-11-09 10:32:59 +0700

    fix redux

--- src/redux/reducers/Index.js
+++ src/redux/reducers/Index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import professional from "./Professional.reducers";
-import booking from "./Booking.reducers";
+// import booking from "./Booking.reducers";
 
 const rootReducers = combineReducers({
   professional,
-  booking,
+  // booking,
 });
 
 export default rootReducers;
