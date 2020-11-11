6c31692a71054591875d69162835819503398b6a
mohammed_azez <37678093+mohammedazez@users.noreply.github.com>
2020-11-11 15:32:42 +0700

    Update Index.js

--- src/redux/reducers/Index.js
+++ src/redux/reducers/Index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from "redux";
 import professional from "./Professional.reducers";
 import user from "./User.reducers";
-import product from "./Product.reducers";
+
 
 const rootReducers = combineReducers({
   professional,
-  user,
-  product,
+  user
 });
 
 export default rootReducers;
