858d13ffbb9f5832d0c1a0ecf855da63692cc0ef
Yogi-123 <myogi.jp@gmail.com>
2020-11-11 14:48:48 +0700

    nambah get userinfo logout get product dan get product by id di redux

--- src/redux/reducers/Index.js
+++ src/redux/reducers/Index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import professional from "./Professional.reducers";
 import user from "./User.reducers";
+import product from "./Product.reducers";
 
 const rootReducers = combineReducers({
   professional,
   user,
+  product,
 });
 
 export default rootReducers;
