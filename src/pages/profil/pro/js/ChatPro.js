a819b45c7807409109016dff1fb552fc0eee5ce2
azezmohammed <muhamadaziz047@gmail.com>
2020-11-21 13:52:25 +0700

    Membuat chat untuk pro dan user

---
+++ src/pages/profil/pro/js/ChatPro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatPro() {
+  return (
+    <div>
+      <h1>Membuat sistem chat untuk pro</h1>
+    </div>
+  );
+}
+
+export default ChatPro;
---
+++ src/pages/profil/user/js/ChatUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatUser() {
+  return (
+    <div>
+      <h1>Membuat sistem chat untuk user</h1>
+    </div>
+  );
+}
+
+export default ChatUser;
