3e79e9277dc451181e506dce61173b4ddfc128ed
azezmohammed <muhamadaziz047@gmail.com>
2020-11-22 20:35:21 +0700

    progress chat for pro

--- src/pages/profil/pro/js/ChatPro.js
+++ src/pages/profil/pro/js/ChatPro.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/ChatPro.css";
 
-function ChatPro() {
+const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+
+let socket;
+
+function ChatPro({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   return (
     <div>
       <h1>Membuat sistem chat untuk pro</h1>
